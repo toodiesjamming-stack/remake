fb6d227c328be8d454e7fc6cb1e0b17aa35e0f9e
wrj80z <major.merritt14@icloud.com>
2026-05-29 21:36:25 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
---This watermark is used to delete the file if its cached, remove it to make the file persist
+--This watermark is used to delete the file if its cached, remove it to make the file persis
 repeat task.wait() until game:IsLoaded()
 if shared.vape then shared.vape:Uninject() end
 
